719e6ad1ee9153d9b0ac9df586807383653a8bed
Aldanis Vigo <aldanisvigo@gmail.com>
2023-02-06 10:11:37 -0600

    first push

---
+++ src/main/java/com/healthpointsfitness/healthpointsfitness/HealthpointsfitnessApplication.java
@@ -0,0 +1,13 @@
+package com.healthpointsfitness.healthpointsfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthpointsfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthpointsfitnessApplication.class, args);
+	}
+
+}

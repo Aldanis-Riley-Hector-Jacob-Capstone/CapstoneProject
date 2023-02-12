6e072b9b725b42e6fce0accb97e3d4efd098dd78
Jacob Jones <jacob.j.jones01@gmail.com>
2023-02-12 14:17:31 -0700

    Got path subscribtion working

--- src/main/java/com/healthpointsfitness/healthpointsfitness/controllers/UserPathViewController.java
+++ src/main/java/com/healthpointsfitness/healthpointsfitness/controllers/UserPathViewController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,6 +24,8 @@ public class UserPathViewController {
     PathsService pathServ;
     @Autowired
     private UserRepository userDao;
+    private Authentication auth;
+    private User curUser;
 
     @GetMapping("/paths/{pathnumber}")
     public String paths02(@PathVariable Long pathnumber, Model model){
@@ -30,19 +33,41 @@ public class UserPathViewController {
             if (pathnumber != null) {
                 Path myPath = pathRepo.getReferenceById(pathnumber);
                 myPath.setImageDataUrl(pathServ.getPathImage(myPath));
-                Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-                User curUser = userDao.findUserByUsername(auth.getName());
+                auth = SecurityContextHolder.getContext().getAuthentication();
+                curUser = userDao.findUserByUsername(auth.getName());
                 model.addAttribute("enrolled", pathServ.isEnrolled(curUser, myPath));
                 model.addAttribute("path", myPath);
                 model.addAttribute("points", pathServ.getTotalPathPoints(myPath));
             } else {
-                return "/profile";
+                return "/users/index";
             }
         } catch (Exception e){
             e.printStackTrace();
         }
 
         return "/users/viewPath";
+    }
+
+    @PostMapping("/paths/enroll/{pathNumber}")
+    public String enrollInPath(@PathVariable Long pathNumber) {
+        Path path = pathRepo.getReferenceById(pathNumber);
+        auth = SecurityContextHolder.getContext().getAuthentication();
+        curUser = userDao.findUserByUsername(auth.getName());
+
+//        USER IS ENROLLED
+        if (pathServ.isEnrolled(curUser, path)) {
+            List<Path> followedPaths = curUser.getFollowed_paths();
+            followedPaths.remove(path);
+            curUser.setFollowed_paths(followedPaths);
+//        USER IS NOT ENROLLED
+        } else {
+            List<Path> followedPaths = curUser.getFollowed_paths();
+            followedPaths.add(path);
+            curUser.setFollowed_paths(followedPaths);
+        }
+
+        userDao.save(curUser);
+        return "redirect:/profile";
     }
 
     @GetMapping("/allPaths")

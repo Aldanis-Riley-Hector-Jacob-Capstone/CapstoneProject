cdf07a362d3fb06069bc523e6caac27f9ab8fee7
Aldanis Vigo <aldanisvigo@gmail.com>
2023-02-17 08:51:23 -0600

    added jitsi back

--- src/main/java/com/healthpointsfitness/healthpointsfitness/HealthpointsfitnessApplication.java
+++ src/main/java/com/healthpointsfitness/healthpointsfitness/HealthpointsfitnessApplication.java
@@ -1,9 +1,11 @@
 package com.healthpointsfitness.healthpointsfitness;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class HealthpointsfitnessApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HealthpointsfitnessApplication.class, args);
